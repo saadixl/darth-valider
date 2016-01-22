45bb49ee888e6659c8b5ffddebcbdb48659cd078
Masudul Haque <hi@masud.pw>
2016-01-22 17:06:19 +0600

    Equals Ignore Case added

--- darth-valider.js
+++ darth-valider.js
@@ -1,15 +1,30 @@
+// valider() is the main function
 function valider(value) {
     this.value = value;
     this.errors = [];
 }
 
-valider.prototype.isValue = function(data) {
+// Checking if two values are equal
+valider.prototype.isEquals = function(data) {
     if (data !== this.value) {
+        if (data.toString().toUpperCase() === this.value.toString().toUpperCase()) {
+            this.errors.push("CaseMismatched");
+        } else {
+            this.errors.push("ValueMismatched");
+        }
+    }
+    return this;
+}
+
+// Checking if two values are equal if case is ignored
+valider.prototype.isEqualsIgnoreCase = function(data) {
+    if (data.toString().toUpperCase() !== this.value.toString().toUpperCase()) {
         this.errors.push("ValueMismatched");
     }
     return this;
 }
 
+// Checking if type of two values are same
 valider.prototype.isType = function(data) {
     if (data !== typeof(this.value)) {
         this.errors.push("TypeMismatched");
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var valider = require('./darth-valider');
-var result = valider("hello").isValue("heilo").isType("number").errors;
+var result = valider(5).isType("number").isEquals(8).errors;
 
 /* Printing if there's any error */
 for (var i = 0; i < result.length; i++) {

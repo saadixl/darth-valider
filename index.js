d297151fcbaac954f05ce791e9c65e1f5cdffa68
saadixl <saadixl@outlook.com>
2016-01-22 13:27:59 +0000

    gonna submit the version 0.1.0

--- index.js
+++
@@ -1,7 +0,0 @@
-var valider = require('./darth-valider');
-var result = valider("               ").isEmpty().errors;
-
-/* Printing if there's any error */
-for (var i = 0; i < result.length; i++) {
-    console.log(result[i] + "\n");
-}

dd1e3006d6f3a91dc5e39da6e492a298c47cbed2
Masudul Haque <hi@masud.pw>
2016-01-22 15:19:31 +0600

    changing implementation

--- darth-valider.js
+++ darth-valider.js
@@ -1,38 +1,5 @@
-module.exports = function(obj) {
-    var matchError = true,
-        matchErrorIC = true,
-        typeError = true;
-
-    /*ALL MATCHING AND CONDITION*/
-    if (generalMatching(obj.input, obj.expected)) {
-        matchError = false;
-    }
-    if (ignoreCaseMatching(obj.input, obj.expected)) {
-        matchErrorIC = false;
-    }
-
-    if (typeMatching(obj.input, obj.expected)) {
-        typeError = false;
-    }
-
-    var result = {
-        "matchError": matchError,
-        "matchErrorIC": matchErrorIC,
-        "typeError": typeError
-    };
-
-    return result;
+function darth(data) {
 
 }
 
-function generalMatching(x, y) {
-    return (x === y);
-}
-
-function ignoreCaseMatching(x, y) {
-    return (x.toString().toUpperCase() === y.toString().toUpperCase());
-}
-
-function typeMatching(x, y) {
-    return (typeof(x) === y);
-}
+module.exports = darth;
--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 var valider = require('./darth-valider');
-var input = {
-    "input": "55",
-    "type": Number,
-    "expected": 55
-};
-var result = valider(input);
 
-console.log(
-    "Input: " + input.input + "\n" +
-    "Expected value: " + input.expected + "\n" +
-    "Expected type: " + input.type + "\n" +
-    "Mismatching error: " + result.matchError + "\n" +
-    "Mismatch after ignoring case: " + result.matchErrorIC + "\n" +
-    "Type matching error: " + result.typeError + "\n"
-);
+valider.setValue("hello").isType(Number).isValue("Helio");
+
